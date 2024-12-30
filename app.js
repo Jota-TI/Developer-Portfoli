4cd54bfa955e0f0849e22dc8e417fa1b08d0e5a9
Jota TI <contatojotati@gmail.com>
2024-12-30 01:58:10 -0300

    Atualizado Html

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// Button to open menu bar
+
+const menuBtn = document.getElementById('menu')
+const menu = document.getElementById('menu-container')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
+
+// Buttons do change theme
+
+const clearTheme = document.getElementById('clear-btn');
+const darkTheme = document.getElementById('dark-btn');
+
+const body = document.getElementById('general-document')
+
+clearTheme.addEventListener('click', () => {
+    body.classList.add('white-theme')
+})
+
+darkTheme.addEventListener('click', () => {
+    body.classList.remove('white-theme')
+})
